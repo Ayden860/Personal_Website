aa7ce034fa6ca0002a12b0bd522639f3a0ec5f53
Ayden860 <134655473+Ayden860@users.noreply.github.com>
2024-04-15 21:01:47 -0700

    Highlighter and animation for the nav/arrow

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -8,15 +9,51 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-
+// getting nav
 nav = document.getElementById('nav');
-
-window.addEventListener('scroll', function() {
-        // console.log(window.scrollY)
-        if(window.scrollY > 990){
-            nav.classList.add('active')
-        }
-        else{
-            nav.classList.remove('active')
-        }
+// Detecting screen scroll to change nav
+window.addEventListener('scroll', function () {
+    console.log(window.scrollY)
+    // scroll y = 666 on home
+    if (window.scrollY > 990) {
+        nav.classList.add('active')
+    }
+    else {
+        nav.classList.remove('active')
+    }
 });
+
+// After clicking link it will highlight the content (total 4 blinks)
+function highlightSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+        section.classList.add('highlight');
+        setTimeout(function () {
+            section.classList.remove('highlight');
+            setTimeout(function () {
+                section.classList.add('highlight');
+                setTimeout(function () {
+                    section.classList.remove('highlight');
+                    setTimeout(function () {
+                        section.classList.add('highlight');
+                        setTimeout(function () {
+                            section.classList.remove('highlight');
+                            setTimeout(function () {
+                                // section.classList.add('highlight');
+                                // setTimeout(function () {
+                                    // section.classList.remove('highlight');
+                                    // setTimeout(function () {
+                                        section.classList.add('highlight');
+                                        setTimeout(function () {
+                                            section.classList.remove('highlight');
+                                        }, 4000);
+                                    // }, 200);
+                                // }, 1000);
+                            }, 200);
+                        }, 1000);
+                    }, 200);
+                }, 1000);
+            }, 200);//Add
+        }, 1000); //Remove
+    }
+}
