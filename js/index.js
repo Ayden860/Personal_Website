06ba89b2393020bec4334debfb6225b8f6f67b47
Ayden860 <134655473+Ayden860@users.noreply.github.com>
2024-05-01 09:47:30 -0700

    update 8

--- js/index.js
+++ js/index.js
@@ -1,17 +1,50 @@
 // getting nav
 nav = document.getElementById('nav');
+// (index)
+home = document.getElementById('home');
+// (project)
+
+// (resume)
+container = document.getElementById('holder')
+// (about me)
+stoper = document.getElementById('stoper')
 // Detecting screen scroll to change nav
+console.log(container)
 window.addEventListener('scroll', function () {
-    console.log(window.scrollY)
-    // scroll y = 666 on home
-    if (window.scrollY > 990) {
-        nav.classList.add('active')
+    // console.log(window.scrollY)
+    // Index page
+    if (this.document.URL.includes("index.html")) {
+        if (window.scrollY > nav.offsetTop + home.offsetHeight) {
+            nav.classList.add('active')
+        }
+        else {
+            nav.classList.remove('active')
+        }
     }
-    else {
-        nav.classList.remove('active')
+    // projects page (not done)
+    // if (window.scrollY > nav.offsetTop + home.offsetHeight) {
+    //     nav.classList.add('active')
+    // }
+    // Resume page 
+    if (this.document.URL.includes("resume.html")) {
+        if (window.scrollY > nav.offsetTop + container.offsetHeight) {
+            nav.classList.add('active')
+        }
+        else {
+            nav.classList.remove('active')
+        }
+    }
+    // About Me
+    if (this.document.URL.includes("aboutMe.html")) {
+        if (window.scrollY > nav.offsetTop + stoper.offsetHeight) {
+            nav.classList.add('active')
+        }
+        else {
+            nav.classList.remove('active')
+        }
     }
 });
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     highlightSection(sectionId)
 })
 // After clicking link it will highlight the content (total 5 blinks)
@@ -20,33 +53,33 @@ function highlightSection(sectionId) {
     if (section) {
         // section.classList.add('highlight');
         // setTimeout(function () {
-            // section.classList.remove('highlight');
-            // setTimeout(function () {
-                // section.classList.add('highlight');
-                // setTimeout(function () {
-                    // section.classList.remove('highlight');
-                    // setTimeout(function () {
-                        // previously commented out
-                        // section.classList.add('highlight');
-                        // setTimeout(function () {
-                            // section.classList.remove('highlight');
-                            // setTimeout(function () {
-                                // section.classList.add('highlight');
-                                // setTimeout(function () {
-                                    // section.classList.remove('highlight');
-                                    // setTimeout(function () {
-                                        section.classList.add('highlight');
-                                        setTimeout(function () {
-                                            section.classList.remove('highlight');
-                                        }, 2500);
-                                    // }, 200);
-                                // }, 1000);
-                            // }, 200);
-                        // }, 1000);
-                    
-                    // }, 200);
-                // }, 1000);
-            //}, 200);//Add
+        // section.classList.remove('highlight');
+        // setTimeout(function () {
+        // section.classList.add('highlight');
+        // setTimeout(function () {
+        // section.classList.remove('highlight');
+        // setTimeout(function () {
+        // previously commented out
+        // section.classList.add('highlight');
+        // setTimeout(function () {
+        // section.classList.remove('highlight');
+        // setTimeout(function () {
+        // section.classList.add('highlight');
+        // setTimeout(function () {
+        // section.classList.remove('highlight');
+        // setTimeout(function () {
+        section.classList.add('highlight');
+        setTimeout(function () {
+            section.classList.remove('highlight');
+        }, 2500);
+        // }, 200);
+        // }, 1000);
+        // }, 200);
+        // }, 1000);
+
+        // }, 200);
+        // }, 1000);
+        //}, 200);//Add
         //}, 1000); //Remove
     }
 }
@@ -55,17 +88,17 @@ function highlightSection(sectionId) {
 const prtColors = document.getElementById('prt-colors')
 const prtWords = document.getElementById('prt-words')
 prtColors.addEventListener('mouseover', () => {
-  // Change the button's background color
-  prtColors.style.opacity = '0.5';
-  prtWords.style.opacity = '1';
+    // Change the button's background color
+    prtColors.style.opacity = '0.5';
+    prtWords.style.opacity = '1';
 });
 // Add a mouseout event listener
 prtColors.addEventListener('mouseout', () => {
-  prtColors.style.opacity = '1';
-  prtWords.style.opacity = '0';
+    prtColors.style.opacity = '1';
+    prtWords.style.opacity = '0';
 });
 prtWords.addEventListener('mouseover', () => {
     // Change the button's background color
     prtColors.style.opacity = '0.5';
     prtWords.style.opacity = '1';
-  });
+});
